29e2bfb59f67769abeefdd0d47bed02a6de86a5b
Gachib <brayan.aruquipa00@gmail.com>
2022-03-13 19:33:53 -0300

    Agregue el codigo del login

--- app.js
+++ app.js
@@ -9,6 +9,22 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '/views/index.html'));
 });
 
+app.get('/productDetail', function(req, res){
+    res.sendFile(path.join(__dirname, '/views/productDetail.html'));
+});
+
+app.get('/productCart', function(req, res){
+    res.sendFile(path.join(__dirname, '/views/productCart.html'));
+});
+
+app.get('/register', function(req, res){
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+});
+
+app.get('/login', function(req, res){
+    res.sendFile(path.join(__dirname, '/views/login.html'));
+});
+
 app.listen(PORT, function(){
     console.log("Servidor levantado en el puerto: " + PORT);
-})
+});
