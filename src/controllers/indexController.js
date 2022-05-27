16a65990b2313e15827a6ddc2b90d9c235dba524
Hernan Gryszkiewicz <rokoposter@yahoo.com.ar>
2022-05-27 01:04:12 -0300

    creacion buscador

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,4 +1,7 @@
 const { getProducts } = require('../data'); 
+const removeAccents = (str) => {
+    return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+}
 
 module.exports ={
     index: (req, res) => {   
@@ -6,5 +9,23 @@ module.exports ={
             productos: getProducts,
             session: req.session
         })
+    },
+    search: (req, res) => { 
+        let buscador = [];
+        getProducts.forEach(producto => {
+            if (removeAccents(producto.name.toLowerCase()).includes(req.query.Busqueda.toLowerCase()) ) {
+                buscador.push(producto)
+                
+            }
+            
+        });
+        res.render('search', {
+           buscador, 
+            busqueda: req.query.busqueda,
+            session: req.session
+
+
+        })
+
     }
 }
--- src/routes/indexRoutes.js
+++ src/routes/indexRoutes.js
@@ -6,7 +6,7 @@ const indexController = require('../controllers/indexController');
 
 
 router.get('/', indexController.index)  
-
+router.get('/search', indexController.search) 
 
 
 module.exports = router
